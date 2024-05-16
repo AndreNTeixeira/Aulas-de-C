b3c8980d7e2bf8ccf79f592f073f31b8e3050572
AndreNTeixeira <163903921+AndreNTeixeira@users.noreply.github.com>
2024-05-16 15:55:25 -0300

    Add files via upload

---
+++ Exercicio 2 Aula 16-05.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int i,j;
+
+int linha, coluna;
+
+int main (){
+
+    printf("Informe as linhas da matriz\n");
+        scanf("%d", &linha);
+    printf("Informe a quantidade de colunas na matriz\n");
+        scanf("%d", &coluna);
+
+int matriz[linha][coluna];
+
+    printf("informe os valores da matriz\n");
+    for(i=0;i<linha;i++){
+        for(j=0;j<linha;j++)
+        scanf("%d", &matriz [i][j]);
+    }
+
+    for(i=0;i<coluna;i++){
+        for(j=0;j<coluna;j++)
+        printf("|%4d", matriz [i][j]);
+        printf("|\n");
+    }
+    return 0;
+}
---
+++ for.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int i,j;
+int matriz[3][3];
+
+int main (){
+
+    for(i=0;i<10;i++){
+        
+        printf("numeros %d\n", i);
+
+    }
+
+    
+
+
+}
